03c577d691c1d7641d326d5218daf975e2a8d906
aleksandrstarzhynskyi <a.stargik@gmail.com>
2024-11-02 20:34:13 +0200

    Added repositories and UnitOfWork

---
+++ OsvitaBackend/OsvitaDAL/Repositories/ChapterRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OsvitaDAL.Data;
+using OsvitaDAL.Entities;
+using OsvitaDAL.Interfaces;
+
+namespace OsvitaDAL.Repositories
+{
+	public class ChapterRepository : Repository<Chapter>, IChapterRepository
+    {
+		public ChapterRepository(OsvitaDbContext context)
+		: base(context)
+		{
+		}
+	}
+}
+
---
+++ OsvitaBackend/OsvitaDAL/Repositories/TopicRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OsvitaDAL.Data;
+using OsvitaDAL.Entities;
+using OsvitaDAL.Interfaces;
+
+namespace OsvitaDAL.Repositories
+{
+	public class TopicRepository : Repository<Topic>, ITopicRepository
+    {
+		public TopicRepository(OsvitaDbContext context)
+		: base(context)
+		{
+		}
+	}
+}
+
