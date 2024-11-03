cb907ed0e84a7f2706bb9edd6b64ac7c6528432f
aleksandrstarzhynskyi <a.stargik@gmail.com>
2024-11-03 12:21:10 +0200

    Added OsvitaAutomapperProfile

---
+++ OsvitaBackend/OsvitaBLL/Configurations/OsvitaAutomapperProfile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using AutoMapper;
+using AutoMapper.Extensions.EnumMapping;
+using OsvitaBLL.Models;
+using OsvitaDAL.Entities;
+
+namespace OsvitaBLL.Configurations
+{
+	public class OsvitaAutomapperProfile : Profile
+	{
+		public OsvitaAutomapperProfile()
+		{
+			CreateMap<Subject, SubjectModel>()
+				.ForMember(sm => sm.ChaptersIds, s => s.MapFrom(x => x.Chapters.Select(c => c.Id)))
+				.ReverseMap();
+
+            CreateMap<Chapter, ChapterModel>()
+				.ForMember(cm => cm.TopicsIds, c => c.MapFrom(x => x.Topics.Select(t => t.Id)))
+				.ReverseMap();
+
+            CreateMap<Topic, TopicModel>()
+                .ForMember(tm => tm.MaterialsIds, t => t.MapFrom(x => x.Materials.Select(m => m.Id)))
+				.ReverseMap();
+
+            CreateMap<Material, MaterialModel>()
+                .ForMember(mm => mm.ContentBlocksIds, m => m.MapFrom(x => x.ContentBlocks.Select(cb => cb.Id)))
+				.ReverseMap();
+
+            CreateMap<ContentBlock, ContentBlockModel>()
+                .ForMember(cbm => cbm.ContentBlockModelType, m => m.MapFrom(x => x.ContentType))
+                .ReverseMap();
+
+            CreateMap<ContentType, ContentBlockModelType>()
+                .ConvertUsingEnumMapping(opt => opt
+                    .MapValue(ContentType.TextBlock, ContentBlockModelType.TextBlock)
+                    .MapValue(ContentType.ImageBlock, ContentBlockModelType.TextBlock)
+                )
+                .ReverseMap();
+        }
+	}
+}
+
