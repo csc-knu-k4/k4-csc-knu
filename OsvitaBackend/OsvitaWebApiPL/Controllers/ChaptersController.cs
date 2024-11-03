bfc5b685a9f6e5571afc3a3b6a3d4e7926c957fb
aleksandrstarzhynskyi <a.stargik@gmail.com>
2024-11-03 17:02:04 +0200

    Created base endpoints

---
+++ OsvitaBackend/OsvitaWebApiPL/Controllers/ChaptersController.cs
@@ -0,0 +1,87 @@
+using Microsoft.AspNetCore.Mvc;
+using OsvitaBLL.Interfaces;
+using OsvitaBLL.Models;
+
+namespace OsvitaWebApiPL.Controllers
+{
+    [Route("api/chapters")]
+    [ApiController]
+    public class ChaptersController : ControllerBase
+    {
+        private readonly IChapterService chapterService;
+        public ChaptersController(IChapterService chapterService)
+        {
+            this.chapterService = chapterService;
+        }
+
+        // GET: api/chapters
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ChapterModel>>> Get()
+        {
+            var chaptersModels = await chapterService.GetAllAsync();
+            if (chaptersModels is not null)
+            {
+                return Ok(chaptersModels);
+            }
+            return NotFound();
+        }
+
+        // GET api/chapters/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ChapterModel>> Get(int id)
+        {
+            var chapterModel = await chapterService.GetByIdAsync(id);
+            if (chapterModel is not null)
+            {
+                return Ok(chapterModel);
+            }
+            return NotFound();
+        }
+
+        // POST api/chapters
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ChapterModel model)
+        {
+            try
+            {
+                await chapterService.AddAsync(model);
+                return Created();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/chapters/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] ChapterModel model)
+        {
+            try
+            {
+                model.Id = id;
+                await chapterService.UpdateAsync(model);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/chapters/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                await chapterService.DeleteByIdAsync(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
