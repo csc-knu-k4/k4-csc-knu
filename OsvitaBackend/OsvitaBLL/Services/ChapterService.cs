6cfd1d2a8e427ccb4b36d9b78af01e97be9ed9ba
aleksandrstarzhynskyi <a.stargik@gmail.com>
2024-11-03 14:04:57 +0200

    Added Services

---
+++ OsvitaBackend/OsvitaBLL/Services/ChapterService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using AutoMapper;
+using OsvitaBLL.Interfaces;
+using OsvitaBLL.Models;
+using OsvitaDAL.Entities;
+using OsvitaDAL.Interfaces;
+
+namespace OsvitaBLL.Services
+{
+	public class ChapterService : IChapterService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IChapterRepository chapterRepository;
+        private readonly IMapper mapper;
+
+        public ChapterService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            chapterRepository = unitOfWork.ChapterRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task AddAsync(ChapterModel model)
+        {
+            var chapter = mapper.Map<Chapter>(model);
+            await chapterRepository.AddAsync(chapter);
+            await unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(ChapterModel model)
+        {
+            await chapterRepository.DeleteByIdAsync(model.Id);
+            await unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            await chapterRepository.DeleteByIdAsync(id);
+            await unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<ChapterModel>> GetAllAsync()
+        {
+            var chapters = await chapterRepository.GetAllAsync();
+            var chaptersModels = mapper.Map<IEnumerable<Chapter>, IEnumerable<ChapterModel>>(chapters);
+            return chaptersModels;
+        }
+
+        public async Task<IEnumerable<ChapterModel>> GetByFilterAsync(FilterSearchModel filterSearchModel)
+        {
+            var chapters = await chapterRepository.GetAllAsync();
+            chapters = chapters?.Where(x =>
+                    (filterSearchModel.SubjectId is null || x.SubjectId == filterSearchModel.SubjectId) &&
+                    (String.IsNullOrEmpty(filterSearchModel.SearchString) || x.Title.Contains(filterSearchModel.SearchString))
+                );
+            var chaptersModels = chapters is not null ? mapper.Map<IEnumerable<Chapter>, IEnumerable<ChapterModel>>(chapters) : new List<ChapterModel>();
+            return chaptersModels;
+        }
+
+        public async Task<ChapterModel> GetByIdAsync(int id)
+        {
+            var chapter = await chapterRepository.GetByIdAsync(id);
+            var chapterModel = mapper.Map<Chapter, ChapterModel>(chapter);
+            return chapterModel;
+        }
+
+        public async Task<IEnumerable<ChapterModel>> GetBySubjectIdAsync(int subjectId)
+        {
+            var chapters = await chapterRepository.GetAllAsync();
+            chapters = chapters?.Where(x => x.SubjectId == subjectId);
+            var chaptersModels = chapters is not null ? mapper.Map<IEnumerable<Chapter>, IEnumerable<ChapterModel>>(chapters) : new List<ChapterModel>();
+            return chaptersModels;
+        }
+
+        public async Task UpdateAsync(ChapterModel model)
+        {
+            var chapter = mapper.Map<Chapter>(model);
+            await chapterRepository.UpdateAsync(chapter);
+            await unitOfWork.SaveChangesAsync();
+        }
+    }
+}
+
---
+++ OsvitaBackend/OsvitaBLL/Services/MaterialService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using AutoMapper;
+using OsvitaBLL.Interfaces;
+using OsvitaBLL.Models;
+using OsvitaDAL.Entities;
+using OsvitaDAL.Interfaces;
+
+namespace OsvitaBLL.Services
+{
+	public class MaterialService : IMaterialService
+	{
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMaterialRepository materialRepository;
+        private readonly IMapper mapper;
+
+        public MaterialService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            materialRepository = unitOfWork.MaterialRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task AddAsync(MaterialModel model)
+        {
+            var material = mapper.Map<Material>(model);
+            await materialRepository.AddAsync(material);
+            await unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(MaterialModel model)
+        {
+            await materialRepository.DeleteByIdAsync(model.Id);
+            await unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            await materialRepository.DeleteByIdAsync(id);
+            await unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<MaterialModel>> GetAllAsync()
+        {
+            var materials = await materialRepository.GetAllAsync();
+            var materialsModels = mapper.Map<IEnumerable<Material>, IEnumerable<MaterialModel>>(materials);
+            return materialsModels;
+        }
+
+        public async Task<IEnumerable<MaterialModel>> GetByFilterAsync(FilterSearchModel filterSearchModel)
+        {
+            var materials = await materialRepository.GetAllWithDetailsAsync();
+            materials = materials?.Where(x =>
+                    (filterSearchModel.SubjectId is null || x.Topic.Chapter.SubjectId == filterSearchModel.SubjectId) &&
+                    (filterSearchModel.ChapterId is null || x.Topic.ChapterId == filterSearchModel.ChapterId) &&
+                    (filterSearchModel.ChapterId is null || x.TopicId == filterSearchModel.TopicId) &&
+                    (String.IsNullOrEmpty(filterSearchModel.SearchString) || x.Title.Contains(filterSearchModel.SearchString))
+                );
+            var materialsModels = materials is not null ? mapper.Map<IEnumerable<Material>, IEnumerable<MaterialModel>>(materials) : new List<MaterialModel>();
+            return materialsModels;
+        }
+
+        public async Task<MaterialModel> GetByIdAsync(int id)
+        {
+            var material = await materialRepository.GetByIdWithDetailsAsync(id);
+            var materialModel = mapper.Map<Material, MaterialModel>(material);
+            return materialModel;
+        }
+
+        public async Task<IEnumerable<MaterialModel>> GetByTopicIdAsync(int topicId)
+        {
+            var materials = await materialRepository.GetAllAsync();
+            materials = materials?.Where(x => x.TopicId == topicId);
+            var topicsModels = materials is not null ? mapper.Map<IEnumerable<Material>, IEnumerable<MaterialModel>>(materials) : new List<MaterialModel>();
+            return topicsModels;
+        }
+
+        public async Task UpdateAsync(MaterialModel model)
+        {
+            var material = mapper.Map<Material>(model);
+            await materialRepository.UpdateAsync(material);
+            await unitOfWork.SaveChangesAsync();
+        }
+    }
+}
+
