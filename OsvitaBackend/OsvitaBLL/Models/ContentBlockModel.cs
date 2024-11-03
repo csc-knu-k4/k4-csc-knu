d21354a02d9c0a718f8a0f07896316256ac43c82
aleksandrstarzhynskyi <a.stargik@gmail.com>
2024-11-03 11:20:13 +0200

    Added models

---
+++ OsvitaBackend/OsvitaBLL/Models/ContentBlockModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace OsvitaBLL.Models
+{
+	public class ContentBlockModel
+	{
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ContentBlockModelType contentBlockModelType { get; set; }
+        public int OrderPosition { get; set; }
+        public int MaterialId { get; set; }
+    }
+}
+
