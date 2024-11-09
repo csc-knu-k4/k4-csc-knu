9c09c61cb5cc829aba597871630ca551eca90119
Vlad <dog792804@gmail.com>
2024-11-09 22:36:20 +0200

    setup frontend

---
+++ OsvitaClient/src/main.tsx
@@ -0,0 +1,15 @@
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<ChakraProvider value={defaultSystem}>
+			<ThemeProvider attribute="class" disableTransitionOnChange>
+				<App />
+			</ThemeProvider>
+		</ChakraProvider>
+	</StrictMode>
+);
