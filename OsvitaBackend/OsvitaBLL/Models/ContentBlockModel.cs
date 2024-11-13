4b912d182abeec2446613d1a6bc1016c66343766
Влад Кучинський <vladkuchinskyi@gmail.com>
2024-11-13 04:34:00 +0200

    added photos upload

--- OsvitaBackend/OsvitaBLL/Models/ContentBlockModel.cs
+++ OsvitaBackend/OsvitaBLL/Models/ContentBlockModel.cs
@@ -1,13 +1,13 @@
-﻿using System;
-namespace OsvitaBLL.Models
+﻿namespace OsvitaBLL.Models
 {
-	public class ContentBlockModel
-	{
+    public class ContentBlockModel
+    {
         public int Id { get; set; }
         public string Title { get; set; }
         public ContentBlockModelType ContentBlockModelType { get; set; }
         public int OrderPosition { get; set; }
         public int MaterialId { get; set; }
+        public string Value { get; set; }
     }
 }
 
---
+++ OsvitaBackend/OsvitaWebApiPL/Controllers/FilesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OsvitaWebApiPL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController(ILogger<FilesController> logger, IWebHostEnvironment environment) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<string>> UploadFile(IFormFile file)
+        {
+            try
+            {
+                if (file == null || file.Length == 0)
+                {
+                    throw new ArgumentException("Invalid file");
+                }
+                var fileName = $"{Guid.NewGuid().ToString()}-{Path.GetExtension(file.FileName)}";
+                var webrootPath = environment.WebRootPath;
+                var path = Path.Combine(webrootPath, "uploads", fileName);
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+                var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}";
+                var fileUrl = $"{baseUrl}/uploads/{fileName}";
+                return fileUrl;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
