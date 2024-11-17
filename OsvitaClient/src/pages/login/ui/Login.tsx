480b2d39e3742ba21d5492e5360f835fb17f899c
Vlad <dog792804@gmail.com>
2024-11-18 01:04:13 +0200

    center layout

--- OsvitaClient/src/pages/login/ui/Login.tsx
+++ OsvitaClient/src/pages/login/ui/Login.tsx
@@ -13,47 +13,49 @@ const Login = () => {
   };
 
   return (
-    <Container maxW="500px" centerContent>
-      <Text color="orange" fontSize="2xl" fontWeight="bold">
-        Вхід
-      </Text>
-      <Field label="Електронна пошта">
-        <Input type="email" variant="flushed" placeholder="Введіть електронну пошту" />
-      </Field>
-      <Field label="Пароль" mt={6}>
-        <Input type="password" variant="flushed" placeholder="Введіть пароль" />
-      </Field>
-      <Flex my={7} w="full" flexDir="row" justifyContent="space-between" alignItems="center">
-        <Checkbox color="gray" colorPalette="orange">
-          Запам’ятати мене
-        </Checkbox>
-        <Text
-          color="orange"
-          _hover={{
-            textDecoration: 'underline',
-          }}
-        >
-          Забули пароль?
+    <Flex h="100vh" w="100vw" justifyContent="center" alignItems="center" bg="gray.50">
+      <Container maxW="500px" centerContent>
+        <Text mb={6} color="orange" fontSize="2xl" fontWeight="bold">
+          Вхід
         </Text>
-      </Flex>
-      <Button fontSize="lg" w="full" borderRadius="1.5rem" bgColor="orange">
-        Увійти
-      </Button>
-      <Text onClick={handleRegister} color="gray" mt={6}>
-        Не маєте акаунту?{' '}
-        <Highlight
-          query="Створити акаунт"
-          styles={{
-            color: 'orange',
-            _hover: {
+        <Field label="Електронна пошта">
+          <Input type="email" variant="flushed" placeholder="Введіть електронну пошту" />
+        </Field>
+        <Field label="Пароль" mt={6}>
+          <Input type="password" variant="flushed" placeholder="Введіть пароль" />
+        </Field>
+        <Flex my={7} w="full" flexDir="row" justifyContent="space-between" alignItems="center">
+          <Checkbox color="gray" colorPalette="orange">
+            Запам’ятати мене
+          </Checkbox>
+          <Text
+            color="orange"
+            _hover={{
               textDecoration: 'underline',
-            },
-          }}
-        >
-          Створити акаунт
-        </Highlight>
-      </Text>
-    </Container>
+            }}
+          >
+            Забули пароль?
+          </Text>
+        </Flex>
+        <Button fontSize="lg" w="full" borderRadius="1.5rem" bgColor="orange">
+          Увійти
+        </Button>
+        <Text onClick={handleRegister} color="gray" mt={6}>
+          Не маєте акаунту?{' '}
+          <Highlight
+            query="Створити акаунт"
+            styles={{
+              color: 'orange',
+              _hover: {
+                textDecoration: 'underline',
+              },
+            }}
+          >
+            Створити акаунт
+          </Highlight>
+        </Text>
+      </Container>
+    </Flex>
   );
 };
 
--- OsvitaClient/src/pages/register/ui/Register.tsx
+++ OsvitaClient/src/pages/register/ui/Register.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { Container, Text, Highlight } from '@chakra-ui/react';
+import { Container, Text, Highlight, Flex } from '@chakra-ui/react';
 import { Input } from '@chakra-ui/react';
 import { Field } from '@/components/ui/field';
 import { useNavigate } from 'react-router-dom';
@@ -12,40 +12,42 @@ const Register = () => {
   };
 
   return (
-    <Container maxW="500px" centerContent>
-      <Text color="orange" fontSize="2xl" fontWeight="bold">
-        Реєстрація
-      </Text>
-      <Field label="Електронна пошта">
-        <Input variant="flushed" placeholder="Введіть електронну пошту" />
-      </Field>
-      <Field label="Пароль" mt={6}>
-        <Input variant="flushed" placeholder="Введіть пароль" />
-      </Field>
-      <Field label="Ім’я" mt={6}>
-        <Input variant="flushed" placeholder="Введіть ім’я" />
-      </Field>
-      <Field label="Прізвище" mt={6}>
-        <Input variant="flushed" placeholder="Введіть прізвище" />
-      </Field>
-      <Button mt={6} fontSize="lg" w="full" borderRadius="1.5rem" bgColor="orange">
-        Зареєструватись
-      </Button>
-      <Text onClick={handleLogin} color="gray" mt={6}>
-        Вже маєте акаунт?{' '}
-        <Highlight
-          query="Увійти"
-          styles={{
-            color: 'orange',
-            _hover: {
-              textDecoration: 'underline',
-            },
-          }}
-        >
-          Увійти
-        </Highlight>
-      </Text>
-    </Container>
+    <Flex h="100vh" w="100vw" justifyContent="center" alignItems="center" bg="gray.50">
+      <Container maxW="500px" centerContent>
+        <Text mb={6} color="orange" fontSize="2xl" fontWeight="bold">
+          Реєстрація
+        </Text>
+        <Field label="Електронна пошта">
+          <Input variant="flushed" placeholder="Введіть електронну пошту" />
+        </Field>
+        <Field label="Пароль" mt={6}>
+          <Input variant="flushed" placeholder="Введіть пароль" />
+        </Field>
+        <Field label="Ім’я" mt={6}>
+          <Input variant="flushed" placeholder="Введіть ім’я" />
+        </Field>
+        <Field label="Прізвище" mt={6}>
+          <Input variant="flushed" placeholder="Введіть прізвище" />
+        </Field>
+        <Button mt={6} fontSize="lg" w="full" borderRadius="1.5rem" bgColor="orange">
+          Зареєструватись
+        </Button>
+        <Text onClick={handleLogin} color="gray" mt={6}>
+          Вже маєте акаунт?{' '}
+          <Highlight
+            query="Увійти"
+            styles={{
+              color: 'orange',
+              _hover: {
+                textDecoration: 'underline',
+              },
+            }}
+          >
+            Увійти
+          </Highlight>
+        </Text>
+      </Container>
+    </Flex>
   );
 };
 
