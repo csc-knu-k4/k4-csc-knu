b115cfd143087cf548af0d83d54faa1dffe42159
aleksandrstarzhynskyi <a.stargik@gmail.com>
2025-03-14 17:37:26 +0200

    Added image field for assignments

--- OsvitaBackend/OsvitaDAL/Entities/Answer.cs
+++ OsvitaBackend/OsvitaDAL/Entities/Answer.cs
@@ -3,6 +3,7 @@
     public class Answer : BaseEntity
     {
         public string Value { get; set; }
+        public string? ValueImage { get; set; }
         public bool IsCorrect { get; set; }
         public int AssignmentId { get; set; }
         public int? Points { get; set; }
