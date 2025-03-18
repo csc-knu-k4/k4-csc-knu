ee0d28bb666e186b75539c7dea5442bfc2336313
buryanochka.06@gmail.com <yana.utochkina@knu.ua>
2025-03-19 01:18:11 +0200

    Added AssigmentSet to EducationPlan (Entity)

--- OsvitaBackend/OsvitaDAL/Entities/AssignmentSetPlanDetail.cs
+++ OsvitaBackend/OsvitaDAL/Entities/AssignmentSetPlanDetail.cs
@@ -1,10 +1,13 @@
-﻿using System;
-namespace OsvitaDAL.Entities
+﻿namespace OsvitaDAL.Entities
 {
 	public class AssignmentSetPlanDetail : BaseEntity
 	{
 		public int? EducationClassPlanId { get; set; }
 		public int AssignmentSetId { get; set; }
+		public int? EducationPlanId { get; set; }
+		public EducationClassPlan? EducationClassPlan { get; set; }
+		// public AssignmentSet AssignmentSet { get; set; }
+		public EducationPlan? EducationPlan { get; set; }
 	}
 }
 
