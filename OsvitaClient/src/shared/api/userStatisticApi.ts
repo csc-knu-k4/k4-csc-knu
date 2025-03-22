3d5df86b1e5454420ea4ce0644c765c90964dc7b
Vlad <dog792804@gmail.com>
2025-03-22 20:15:14 +0200

    add new endpoints

--- OsvitaClient/src/shared/api/userStatisticApi.ts
+++ OsvitaClient/src/shared/api/userStatisticApi.ts
@@ -9,3 +9,13 @@ export const addStatisticAssignments = async (userId: number, assignments: any) 
   const response = await api.post(`/users/${userId}/statistic/assignments`, assignments);
   return response.data;
 };
+
+export const addStatisticTopic = async (userId: number, topic: any) => {
+  const response = await api.post(`/users/${userId}/statistic/topics`, topic);
+  return response.data;
+};
+
+export const updateStatisticTopic = async (userId: number, topic: any) => {
+  const response = await api.put(`/users/${userId}/statistic/topics`, topic);
+  return response.data;
+};
