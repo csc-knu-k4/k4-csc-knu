ce089f17238e2312cf69db0a2e7f4c7f3c9d1046
Vlad <dog792804@gmail.com>
2025-03-27 19:52:14 +0200

    save

--- OsvitaClient/src/pages/courses/subjectTaskTest/ui/SingleChoiceQuestion.tsx
+++ OsvitaClient/src/pages/courses/subjectTaskTest/ui/SingleChoiceQuestion.tsx
@@ -44,7 +44,7 @@ const SingleChoiceQuestion = ({
       </Text>
       {data.problemDescriptionImage && (
         <Image
-          src={`${window.location.origin}${data.problemDescriptionImage}`}
+          src={`${'http://localhost:5134'}${data.problemDescriptionImage}`}
           alt="зображення до завдання"
           borderRadius="lg"
           maxW="100%"
