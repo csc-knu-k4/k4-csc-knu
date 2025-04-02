24bbb50c8080130d1f778460cd8da9ab9d4429fa
Vlad <dog792804@gmail.com>
2025-04-02 22:01:58 +0300

    fix

--- OsvitaClient/src/pages/teacher/ClassStudents/ui/ClassStudents.tsx
+++ OsvitaClient/src/pages/teacher/ClassStudents/ui/ClassStudents.tsx
@@ -1,14 +1,52 @@
-import { Button, Flex, VStack, Text, Menu, Portal } from '@chakra-ui/react';
+import {
+  Button,
+  Flex,
+  VStack,
+  Text,
+  Menu,
+  Portal,
+  Popover,
+  IconButton,
+  Input,
+} from '@chakra-ui/react';
 import { LuPlus } from 'react-icons/lu';
 import { HiDotsVertical } from 'react-icons/hi';
 import { UserAvatar } from '@/shared/ui/Avatar';
+import { IoPaperPlane } from 'react-icons/io5';
 
 const ClassStudents = () => {
   return (
     <>
-      <Button borderRadius="1rem" colorPalette="orange">
-        <LuPlus /> Додати учня
-      </Button>
+      <Popover.Root>
+        <Popover.Trigger asChild>
+          <Button borderRadius="1rem" colorPalette="orange">
+            <LuPlus /> Додати учня
+          </Button>
+        </Popover.Trigger>
+        <Portal>
+          <Popover.Positioner>
+            <Popover.Content css={{ '--popover-bg': 'white' }}>
+              <Popover.Arrow />
+              <Popover.Body>
+                <Popover.Title fontWeight="medium" color="black" fontSize="md">
+                  Запросити учня
+                </Popover.Title>
+                <Flex flexDir="row" justifyContent="center" alignItems="center" mt={4}>
+                  <Input
+                    colorPalette="orange"
+                    borderRadius="1rem"
+                    placeholder="Введіть електронну пошту"
+                    size="md"
+                  />
+                  <IconButton borderRadius="1rem" aria-label="Invite user" variant="ghost">
+                    <IoPaperPlane color="#f97316" size={28} />
+                  </IconButton>
+                </Flex>
+              </Popover.Body>
+            </Popover.Content>
+          </Popover.Positioner>
+        </Portal>
+      </Popover.Root>
       <VStack gap={4} mt={6}>
         <Flex
           borderRadius="1rem"
