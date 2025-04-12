209ec2b04dfe9faf4a4a2b75cfd163622f66e078
Vlad <dog792804@gmail.com>
2025-04-12 21:37:25 +0300

    save

--- OsvitaClient/src/pages/profile/ui/Profile.tsx
+++ OsvitaClient/src/pages/profile/ui/Profile.tsx
@@ -3,6 +3,8 @@ import { Badge, Button, Flex, Text, Container } from '@chakra-ui/react';
 import { CiCalendar } from 'react-icons/ci';
 import { FaArrowTrendUp } from 'react-icons/fa6';
 import { PiMedal } from 'react-icons/pi';
+import { FaRegCheckCircle } from 'react-icons/fa';
+import { Avatar } from '@/components/ui/avatar';
 const Profile = () => {
   return (
     <Container centerContent>
@@ -17,7 +19,7 @@ const Profile = () => {
             h="270px"
             w="550px"
           >
-            <UserAvatar />
+            <Avatar size="2xl" />
             <Text fontSize="xl" my={4} fontWeight="bold">
               Костюк Світлана
             </Text>
@@ -38,14 +40,20 @@ const Profile = () => {
               fontWeight="semibold"
               colorPalette="orange"
             >
-              <CiCalendar />
+              <CiCalendar size="1.5rem" />
               Щоденні Завдання
             </Button>
-            <Flex>
-              <Text color="gray">Сьогоднішнє завдання виконане</Text>
-              <Text fontSize="lg" fontWeight="semibold" color="orange">
-                <PiMedal /> 345 балів
-              </Text>
+            <Flex flexDir="row" justifyContent="space-between" alignItems="center">
+              <Flex gap={1} flexDir="row" alignItems="center" justifyContent="center" mb={5}>
+                <FaRegCheckCircle size="1.5rem" color="green" />
+                <Text color="gray">Сьогоднішнє завдання виконане</Text>
+              </Flex>
+              <Flex gap={1} flexDir="row" alignItems="center" justifyContent="center" mb={5}>
+                <PiMedal size="1.5rem" color="rgb(255, 109, 24)" />
+                <Text fontSize="lg" fontWeight="semibold" color="orange">
+                  345 балів
+                </Text>
+              </Flex>
             </Flex>
           </Flex>
         </Flex>
@@ -57,38 +65,151 @@ const Profile = () => {
           fontWeight="semibold"
           h="450px"
           w="550px"
-          overflowY="auto"
           flexDir="column"
         >
-          <Text color="orange" mb={5}>
-            <FaArrowTrendUp />
-            Рейтинг
-          </Text>
-          <Flex
-            borderRadius="1rem"
-            borderColor="orange"
-            borderWidth="1px"
-            flexDir="row"
-            flexWrap="nowrap"
-            justifyContent="space-between"
-            alignItems="center"
-            h="45px"
-            px={4}
-          >
-            <Flex flexDir="row" alignItems="center" gap={2}>
-              <Text color="orange.200" fontSize="lg">
-                1
-              </Text>
-              <UserAvatar />
-              <Text color="orange.200" fontSize="lg">
-                Костюк Світлана
-              </Text>
+          <Flex gap={2} flexDir="row" alignItems="center" justifyContent="center" mb={5}>
+            <FaArrowTrendUp color="rgb(255, 109, 24)" />
+            <Text color="orange">Рейтинг</Text>
+          </Flex>
+          <Flex gap={2} flexDir="column" overflowY="auto" h="200px">
+            <Flex
+              borderRadius="1rem"
+              borderColor="orange"
+              borderWidth="1px"
+              flexDir="row"
+              flexWrap="nowrap"
+              justifyContent="space-between"
+              alignItems="center"
+              h="45px"
+              px={4}
+            >
+              <Flex flexDir="row" alignItems="center" gap={2}>
+                <Text color="orange.200" fontSize="lg">
+                  1
+                </Text>
+                <UserAvatar />
+                <Text color="orange.200" fontSize="lg">
+                  Костюк Світлана
+                </Text>
+              </Flex>
+              <Flex gap={1} flexDir="row" alignItems="center" justifyContent="center">
+                <Text fontSize="md" fontWeight="semibold" color="orange.200">
+                  345 балів
+                </Text>
+                <PiMedal color="rgb(255, 109, 24)" />
+              </Flex>
             </Flex>
-            <Flex flexDir="row" alignItems="center">
-              <Text fontSize="md" fontWeight="semibold" color="orange.200">
-                345 балів
-              </Text>
-              <PiMedal color="rgb(255, 109, 24)" />
+
+            <Flex
+              borderRadius="1rem"
+              borderColor="orange"
+              borderWidth="1px"
+              flexDir="row"
+              flexWrap="nowrap"
+              justifyContent="space-between"
+              alignItems="center"
+              h="45px"
+              px={4}
+            >
+              <Flex flexDir="row" alignItems="center" gap={2}>
+                <Text color="orange.200" fontSize="lg">
+                  2
+                </Text>
+                <UserAvatar />
+                <Text color="orange.200" fontSize="lg">
+                  Костюк Світлана
+                </Text>
+              </Flex>
+              <Flex gap={1} flexDir="row" alignItems="center" justifyContent="center">
+                <Text fontSize="md" fontWeight="semibold" color="orange.200">
+                  345 балів
+                </Text>
+                <PiMedal color="rgb(255, 109, 24)" />
+              </Flex>
+            </Flex>
+
+            <Flex
+              borderRadius="1rem"
+              borderColor="orange"
+              borderWidth="1px"
+              flexDir="row"
+              flexWrap="nowrap"
+              justifyContent="space-between"
+              alignItems="center"
+              h="45px"
+              px={4}
+            >
+              <Flex flexDir="row" alignItems="center" gap={2}>
+                <Text color="orange.200" fontSize="lg">
+                  3
+                </Text>
+                <UserAvatar />
+                <Text color="orange.200" fontSize="lg">
+                  Костюк Світлана
+                </Text>
+              </Flex>
+              <Flex gap={1} flexDir="row" alignItems="center" justifyContent="center">
+                <Text fontSize="md" fontWeight="semibold" color="orange.200">
+                  345 балів
+                </Text>
+                <PiMedal color="rgb(255, 109, 24)" />
+              </Flex>
+            </Flex>
+
+            <Flex
+              borderRadius="1rem"
+              borderColor="orange"
+              borderWidth="1px"
+              flexDir="row"
+              flexWrap="nowrap"
+              justifyContent="space-between"
+              alignItems="center"
+              h="45px"
+              px={4}
+            >
+              <Flex flexDir="row" alignItems="center" gap={2}>
+                <Text color="orange.200" fontSize="lg">
+                  4
+                </Text>
+                <UserAvatar />
+                <Text color="orange.200" fontSize="lg">
+                  Костюк Світлана
+                </Text>
+              </Flex>
+              <Flex gap={1} flexDir="row" alignItems="center" justifyContent="center">
+                <Text fontSize="md" fontWeight="semibold" color="orange.200">
+                  345 балів
+                </Text>
+                <PiMedal color="rgb(255, 109, 24)" />
+              </Flex>
+            </Flex>
+
+            <Flex
+              borderRadius="1rem"
+              borderColor="orange"
+              borderWidth="1px"
+              flexDir="row"
+              flexWrap="nowrap"
+              justifyContent="space-between"
+              alignItems="center"
+              h="45px"
+              px={4}
+            >
+              <Flex flexDir="row" alignItems="center" gap={2}>
+                <Text color="orange.200" fontSize="lg">
+                  5
+                </Text>
+                <UserAvatar />
+                <Text color="orange.200" fontSize="lg">
+                  Костюк Світлана
+                </Text>
+              </Flex>
+              <Flex gap={1} flexDir="row" alignItems="center" justifyContent="center">
+                <Text fontSize="md" fontWeight="semibold" color="orange.200">
+                  345 балів
+                </Text>
+                <PiMedal color="rgb(255, 109, 24)" />
+              </Flex>
             </Flex>
           </Flex>
         </Flex>
